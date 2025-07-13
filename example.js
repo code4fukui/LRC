9b25c7bcc1c621bc0b42315914aafd643089099a
Taisuke Fukuno <fukuno@jig.jp>
2025-07-13 14:54:23 +0900

    added a sample of LRC

--- example.js
+++ example.js
@@ -2,7 +2,10 @@ import { CSV } from "https://js.sabae.cc/CSV.js";
 import { LRC } from "./LRC.js";
 
 const fn = "theme-of-kawada-elementary-school.lyrics.csv";
+const dstfn = "theme-of-kawada-elementary-school.lrc";
+
 const lyrics = await CSV.fetchJSON(fn);
 const lrc = LRC.stringify({ title: "河和田小学校のテーマ", lyrics });
 console.log(lrc);
+await Deno.writeTextFile(dstfn, lrc);
 
